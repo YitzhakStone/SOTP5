753e8f67d57ff0845a01342f48f40a30206c08e0
Yitzhak Stone <andrade@hotmail.com.br>
2015-04-16 00:21:43 -0300

    Exercício 2.1

---
+++ TP/EX_2_1/EX_2_1.cs
@@ -0,0 +1,101 @@
+/*  
+
+Mateus Fernando			xxxxxx
+Vinicius Ponciano		xxxxxx
+Yitzhak Stone			478493
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+            	Console.WriteLine("{0}\t\t{1}", "xxxxxx", "Mateus Fernando");
+            	Console.WriteLine("{0}\t\t{1}", "xxxxxx", "Vinicius Ponciano");
+            	Console.WriteLine("{0}\t\t{1}", "478493", "Yitzhak Stone");
+
+            	Console.WriteLine();
+
+                decimal n1, n2, r;
+
+                if (args.Length != 3 ||
+                    (!(decimal.TryParse(args[0], out n1) && decimal.TryParse(args[2], out n2))) ||
+                    (!(new string[] { "+", "-", "x", "/", "*" }.Contains(args[1]))))
+                {
+                    Console.WriteLine("Parametros Inválidos");
+                    return;
+                }
+
+                string operador = args[1];
+
+                switch (operador)
+                {
+                    case "+":
+                        r = adicao(n1, n2);
+                        break;
+                    case "-":
+                        r = subtracao(n1, n2);
+                        break;
+                    case "x":
+                    case "*":
+                        r = multiplicacao(n1, n2);
+                        break;
+                    case "/":
+                        r = divisao(n1, n2);
+                        break;
+                    default:
+                        r = 0;
+                        break;
+                }
+
+                Console.Write("{0} {1} {2} = {3}", n1.ToString(), operador.ToString(), n2.ToString(), r.ToString());
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+
+        }
+
+        static decimal adicao(decimal n1, decimal n2)
+        {
+            return  (n1 + n2);
+        }
+
+        static decimal subtracao(decimal n1, decimal n2)
+        {
+            return (n1 - n2);
+        }
+
+        static decimal multiplicacao(decimal n1, decimal n2)
+        {
+            return (n1 * n2);
+        }
+
+        static decimal divisao(decimal n1, decimal n2)
+        {            
+            
+            if (n2 == 0)
+            {
+                throw new Exception("Tentativa de divisão por zero.");
+            }
+
+            return n1 / n2;
+        }
+
+    }
+}
